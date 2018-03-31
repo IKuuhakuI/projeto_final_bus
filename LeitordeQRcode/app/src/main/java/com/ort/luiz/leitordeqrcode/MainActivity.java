70d46ab7ccbf98e80cf180e63eb7d58647e49438
Luiz-Sama <themax285gamer@gmail.com>
2018-03-31 00:00:12 -0300

    Corrige bug ao ler o mesmo QR duas vezes seguidas

--- LeitordeQRcode/app/src/main/java/com/ort/luiz/leitordeqrcode/MainActivity.java
+++ LeitordeQRcode/app/src/main/java/com/ort/luiz/leitordeqrcode/MainActivity.java
@@ -9,15 +9,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+
+import static java.lang.String.valueOf;
 
 public class MainActivity extends AppCompatActivity {
     Button btnScan;
     private String onibus1;
-    private String onibus2;
 
     FirebaseDatabase database;
     DatabaseReference myRef;
@@ -33,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
 
         final Activity activity = this;
 
+        myRef = database.getReference("Onibus1");
+
         btnScan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -43,6 +49,8 @@ public class MainActivity extends AppCompatActivity {
                 integrator.initiateScan();
             }
         });
+
+        alert(onibus1);
     }
 
     @Override
@@ -50,16 +58,16 @@ public class MainActivity extends AppCompatActivity {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
 
         if(result != null){
-            if(result.getContents() != null){
-                myRef = database.getReference("Onibus1");
+            if(result.getContents() != null) {
                 myRef.child("QR").setValue(result.getContents());
-                myRef.child("Rota").child(result.getContents()).child("Position").setValue(1);
 
-                if(onibus1 != null){
+                if (onibus1 != null){
                     myRef.child("Rota").child(onibus1).child("Position").setValue(0);
                 }
 
+                myRef.child("Rota").child(result.getContents()).child("Position").setValue(1);
                 onibus1 = result.getContents();
+
             } else{
                 alert("Scan cancelado");
             }
