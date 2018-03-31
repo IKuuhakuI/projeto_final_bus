12bc8ee3604bdbbc5a1075bab5ea43dee9207916
Luiz-Sama <themax285gamer@gmail.com>
2018-03-31 17:37:32 -0300

    Corrige bug de dois Pontos com Position = 1 e renomeia LeitordeQRcode para onibus1

---
+++ onibus1/app/src/main/java/com/ort/luiz/leitordeqrcode/MainActivity.java
@@ -0,0 +1,94 @@
+package com.ort.luiz.leitordeqrcode;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.wifi.ScanResult;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import static java.lang.String.valueOf;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnScan;
+    private String onibus1;
+    private String qr;
+
+    FirebaseDatabase database;
+    DatabaseReference onibus1Ref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        database = FirebaseDatabase.getInstance();
+
+        btnScan = findViewById(R.id.btnScan);
+
+        final Activity activity = this;
+
+        onibus1Ref = database.getReference("Onibus1");
+
+        onibus1Ref.child("QR").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                qr = dataSnapshot.getValue().toString();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setPrompt("Camera Scan");
+                integrator.setCameraId(0);
+                integrator.initiateScan();
+            }
+        });
+
+        alert(onibus1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(result != null){
+            if(result.getContents() != null) {
+                onibus1Ref.child("QR").setValue(result.getContents());
+                if (onibus1 != null){
+                    onibus1Ref.child("Rota").child(onibus1).child("Position").setValue(0);
+                } else{
+                    onibus1Ref.child("Rota").child(qr).child("Position").setValue(0);
+                }
+                onibus1Ref.child("Rota").child(result.getContents()).child("Position").setValue(1);
+                onibus1 = result.getContents();
+
+            } else{
+                alert("Scan cancelado");
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+    
+    private void alert(String msg){
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+    }
+}
