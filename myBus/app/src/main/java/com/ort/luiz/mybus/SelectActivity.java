7f264211f8c1e83e80498b89b8a04d41fff2ce1a
Luiz-Sama <themax285gamer@gmail.com>
2018-06-18 10:50:02 -0300

    Adiciona a página para selecionar o tipo de rastreamento

---
+++ myBus/app/src/main/java/com/ort/luiz/mybus/SelectActivity.java
@@ -0,0 +1,23 @@
+package com.ort.luiz.mybus;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SelectActivity extends AppCompatActivity {
+
+    Button btnOnibus, btnRastrear;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select);
+        btnOnibus = findViewById(R.id.btnOnibusId);
+        btnRastrear = findViewById(R.id.btnRastrearID);
+
+        btnOnibus.setOnClickListener(V->{
+            startActivity(new Intent(this, FirstPageActivity.class));
+        });
+    }
+}
